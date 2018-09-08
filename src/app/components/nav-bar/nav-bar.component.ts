0686019300866e72b6d38c32e966c6d78e7f6db8
MEIGNIEZ <guillaume.meigniez@gmail.com>
2018-09-08 13:51:01 +0200

    navbar web & mobile without animations

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  menuDisplayed = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  displayMenu() {
+    const menuElementsContainers = document.getElementsByClassName('nav-li-item');
+    const menuElements = document.getElementsByClassName('nav-item');
+    if (!this.menuDisplayed) {
+      this.menuDisplayed = true;
+
+      for (let i = 0; i < menuElementsContainers.length; i++) {
+        menuElementsContainers[i]['style'].backgroundColor = '#f4f4f4';
+      }
+
+      for (let i = 0; i < menuElements.length; i++) {
+        menuElements[i]['style'].visibility = 'visible';
+      }
+    } else {
+      this.menuDisplayed = false;
+      for (let i = 0; i < menuElementsContainers.length; i++) {
+        menuElementsContainers[i]['style'].backgroundColor = 'transparent';
+      }
+
+      for (let i = 0; i < menuElements.length; i++) {
+        menuElements[i]['style'].visibility = 'hidden';
+      }
+    }
+  }
 }
